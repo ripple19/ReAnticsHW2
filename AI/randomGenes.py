8efd9e1d2d83eda83d0bb9ccecff8dcc7f16be54
Andrew Ripple <ripple19@up.edu>
2018-11-06 01:57:20 -0800

    Added some stuff

---
+++ AI/randomGenes.py
@@ -0,0 +1,175 @@
+import random
+import sys
+
+sys.path.append("..")  # so other modules can be found in parent dir
+from Player import *
+from Constants import *
+from Construction import CONSTR_STATS
+from Ant import UNIT_STATS
+from Move import Move
+from GameState import *
+from AIPlayerUtils import *
+
+
+##
+# AIPlayer
+# Description: The responsbility of this class is to interact with the game by
+# deciding a valid move based on a given game state. This class has methods that
+# will be implemented by students in Dr. Nuxoll's AI course.
+#
+# Variables:
+#   playerId - The id of the player.
+##
+class AIPlayer(Player):
+    # __init__
+    # Description: Creates a new Player
+    #
+    # Parameters:
+    #   inputPlayerId - The id to give the new player (int)
+    #   cpy           - whether the player is a copy (when playing itself)
+    ##
+    def __init__(self, inputPlayerId):
+        super(AIPlayer, self).__init__(inputPlayerId, "GeneMaster")
+        self.geneList = None
+        self.currentGene = None
+        self.currentGeneIndex = None
+
+    ##
+    # getPlacement
+    #
+    # Description: called during setup phase for each Construction that
+    #   must be placed by the player.  These items are: 1 Anthill on
+    #   the player's side; 1 tunnel on player's side; 9 grass on the
+    #   player's side; and 2 food on the enemy's side.
+    #
+    # Parameters:
+    #   construction - the Construction to be placed.
+    #   currentState - the state of the game at this point in time.
+    #
+    # Return: The coordinates of where the construction is to be placed
+    ##
+    def getPlacement(self, currentState):
+        numToPlace = 0
+        # implemented by students to return their next move
+        if currentState.phase == SETUP_PHASE_1:  # stuff on my side
+
+            numToPlace = 11
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    # Choose any x location
+                    x = random.randint(0, 9)
+                    # Choose any y location on your side of the board
+                    y = random.randint(0, 3)
+                    # Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        # Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        elif currentState.phase == SETUP_PHASE_2:  # stuff on foe's side
+            numToPlace = 2
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    # Choose any x location
+                    x = random.randint(0, 9)
+                    # Choose any y location on enemy side of the board
+                    y = random.randint(6, 9)
+                    # Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        # Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        else:
+            return [(0, 0)]
+
+    ##
+    # getMove
+    # Description: Gets the next move from the Player.
+    #
+    # Parameters:
+    #   currentState - The state of the current game waiting for the player's move (GameState)
+    #
+    # Return: The Move to be made
+    ##
+    def getMove(self, currentState):
+        moves = listAllLegalMoves(currentState)
+        selectedMove = moves[random.randint(0, len(moves) - 1)];
+
+        # don't do a build move if there are already 3+ ants
+        numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
+        while (selectedMove.moveType == BUILD and numAnts >= 3):
+            selectedMove = moves[random.randint(0, len(moves) - 1)];
+
+        return selectedMove
+
+    ##
+    # getAttack
+    # Description: Gets the attack to be made from the Player
+    #
+    # Parameters:
+    #   currentState - A clone of the current state (GameState)
+    #   attackingAnt - The ant currently making the attack (Ant)
+    #   enemyLocation - The Locations of the Enemies that can be attacked (Location[])
+    ##
+    def getAttack(self, currentState, attackingAnt, enemyLocations):
+        # Attack a random enemy.
+        return enemyLocations[random.randint(0, len(enemyLocations) - 1)]
+
+    ##
+    # registerWin
+    #
+    # This agent doens't learn
+    #
+    def registerWin(self, hasWon):
+        # method templaste, not implemented
+        pass
+
+    def registerWin(self, hasWon):
+        # self.currentGene.fitness = fitnesstest(self.currentGene.dna)
+        # Add current fitness score to the average fitness score. (Divide by number of games after done)
+        # if number of games is past a current number, then were good.
+        # Check to see if there are more genees. If so, advance to next gene.
+        # If we are out of genes in this pop, then create a new population. Reset our index to start.
+        # Update our list of genes to the new one, start playing again.
+        # Else, we want to keep testing this gene, play again.
+        pass
+
+    # Method to take two parents from the gene population and
+    # create two children. Will call the fitness function to figure out which is best.
+    def createNewPopulation(self, firstGene, secondGene):
+        pass
+
+    def nextGeneration(self, listOfGenes):
+        lowFitness = 0
+        fitGenes = None
+        for gene in listOfGenes:
+            if gene.fitness > lowFitness:
+                fitGenes.add(gene)
+
+                # Need to flush out the original gene list
+                # Call gene function that mates two parents. mate(gene1, gene2)
+                # Return two new child genes. Add them to the new gene list.
+                # Set new generation to be the actual gene list.
+
+    def fitnessTest(self, gene):
+        pass
+
+
+class Gene:
+    def __init__(self):
+        self.DNA = None
+        self.currentFitness = None
+        self.isParent = False
+        self.isChild = False
+        self.averageFitness = None
+        self.numGamesPlayed = None
+
+        # Look at a gene and determine its fitness according to a predefined set of tests.
+        # This method
